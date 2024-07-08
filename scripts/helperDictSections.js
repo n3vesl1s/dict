d78dd16cc50d21e8a36b0b818678415d325ce1f9
n3vesl1s <159696106+n3vesl1s@users.noreply.github.com>
2024-07-08 13:42:59 +0300

    Fixing 1.75

--- scripts/helperDictSections.js
+++ scripts/helperDictSections.js
@@ -110,7 +110,7 @@ function changeExpState(id) {
       defention.innerHTML = key2 + " - " + value2;
       defention.onclick = function() {
         navigator.clipboard.writeText(key2 + " - " + value2 + " (https://n3vesl1s.github.io/dict/)");
-        const copyAlert = document.getElementById("copyMessage");
+        const copyAlert = document.getElementById("copyAlert");
         copyAlert.style.display = "block";
         console.log ("visible")
         setTimeout(() => {
